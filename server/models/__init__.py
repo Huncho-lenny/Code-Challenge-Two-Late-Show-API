0635a55d67bbe380a30fc91b5c0fc46b2710ce69
lenny <lenny.kimanthi@student.moringaschool.com>
2025-06-23 23:05:26 +0300

    edited

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,4 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+
+db = SQLAlchemy()
+SerializerMixin = SerializerMixin
+
 from .user import User
 from .guest import Guest
 from .episode import Episode
 from .appearance import Appearance
+from .token_blocklist import TokenBlocklist
--- server/models/user.py
+++ server/models/user.py
@@ -1,28 +1,20 @@
-from sqlalchemy.orm import validates
-from config import db
+from . import db, SerializerMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
-    __tablename__ = "users"
+class User(db.Model, SerializerMixin):
+    __tablename__= "users"
+
+    serialize_rules = ('-appearances.user', '-password_hash',)
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
-    _password_hash = db.Column(db.String, nullable=False)
+    password_hash = db.Column(db.String, nullable=False)
 
-    def __init__(self, username, password):
-        self.username = username
-        self.password_hash = password
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-    @property
-    def password_hash(self):
-        raise AttributeError("Password hashes cannot be viewed.")
-
-    @password_hash.setter
-    def password_hash(self, password):
-        self._password_hash = generate_password_hash(password)
-
-    def authenticate(self, password):
-        return check_password_hash(self._password_hash, password)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
-        return f"<User {self.username}>"
+        return f"<User id={self.id} username={self.username}>" 
